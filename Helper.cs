b6b5c757e160a7c4f4696d4a9989112ebfd567f7
factormystic <factormystic@gmail.com>
2013-05-18 15:03:16 -0400

    initial commit

---
+++ Helper.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Scheduler = Microsoft.Win32.TaskScheduler;
+
+namespace FMUtils.TaskRUninstaller
+{
+    public class Helper
+    {
+        public static bool IsTaskInstalled(string name)
+        {
+            using (var ts = new Scheduler.TaskService())
+                return ts.FindTask(name) != null;
+        }
+
+        public static void Install(string self, string action, string name, string path)
+        {
+            if (IsTaskInstalled(name))
+                return;
+
+            using (var ts = new Scheduler.TaskService())
+            {
+                var task = ts.NewTask();
+
+                task.Principal.RunLevel = Scheduler.TaskRunLevel.Highest;
+
+                task.Settings.DisallowStartIfOnBatteries = false;
+                task.Settings.ExecutionTimeLimit = TimeSpan.Zero;
+
+                task.Triggers.Add(new Scheduler.LogonTrigger() { UserId = System.Security.Principal.WindowsIdentity.GetCurrent().Name });
+                task.Actions.Add(new Scheduler.ExecAction(self, action + " " + name + " " + path));
+
+                ts.RootFolder.RegisterTaskDefinition(name, task);
+            }
+        }
+
+        public static void Uninstall(string name)
+        {
+            if (!IsTaskInstalled(name))
+                return;
+
+            using (var ts = new Scheduler.TaskService())
+                ts.RootFolder.DeleteTask(name);
+        }
+
+        public static bool Toggle(string self, string name, string path)
+        {
+            if (IsTaskInstalled(name))
+            {
+                Uninstall(name);
+                return false;
+            }
+            else
+            {
+                Install(self, "runinstall", name, path);
+                return true;
+            }
+        }
+
+        internal static void Suicide(string self, string name)
+        {
+            var ParentDir = Directory.GetParent(self);
+
+            if (ParentDir.Name != name)
+                return;
+
+            Process.Start(new ProcessStartInfo()
+            {
+                FileName = "cmd.exe",
+                Arguments = "/c timeout 5 & rmdir /q /s " + ParentDir.FullName,
+
+                CreateNoWindow = true,
+                WindowStyle = ProcessWindowStyle.Hidden,
+            });
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace FMUtils.TaskRUninstaller
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            var args = Environment.GetCommandLineArgs();
+
+            if (args.Length != 4)
+                return;
+
+            var self = args[0];
+            var action = args[1];
+            var name = args[2];
+            var path = args[3];
+
+            switch (action)
+            {
+                case "install":
+                    // non-task invokation:
+                    // set up a scheduled task named `taskname` that runs `program_path`
+
+                    Helper.Install(self, "rununinstall", name, path);
+                    break;
+
+                case "runinstall":
+                    // task invokation:
+                    // try and run the `program_path`, but if it doesn't exist then uninstall the task and delete runinstaller.exe
+
+                    try
+                    {
+                        Process.Start(path);
+                    }
+                    catch (Win32Exception e)
+                    {
+                        Helper.Uninstall(name);
+                        Helper.Suicide(self, name);
+                    }
+                    break;
+            }
+        }
+    }
+}
